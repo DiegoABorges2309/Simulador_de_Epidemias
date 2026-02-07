c9b99c3a020f4d0dc74b4677e1e2aa4f68abdb62
nelvismar16 <nelvism17@gmail.com>
2026-02-07 16:07:24 -0400

    Update paciente.py

--- models_clases/paciente.py
+++ models_clases/paciente.py
@@ -1,10 +1,13 @@
-
 class Paciente():
     def __init__(self, id_paciente, estado="Sano"):
         self.id = id_paciente
         self.estado = estado
         self.dia_contagio = None
+        self.dias_enfermo = 0
+        self.en_hospital = False
         self.dia_ingreso_cola = None
         self.dia_atencion = None
-        self.dias_enfermo = 0
+        
+
+
 
