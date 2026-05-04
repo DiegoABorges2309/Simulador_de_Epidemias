a4ade3bdcd71e4135db8c9a89f736c8a535520fd
Diego~ <udiegoborgesdev@gmail.com>
2026-05-03 20:26:37 -0400

    inicio de pruebas para simulacion del dengue

--- modelos/simulacion.py
+++
@@ -1,9 +0,0 @@
-
-class Simulacion():
-    def __init__(self, poblacion_total, cant_contagio_inicial, area_km2, capacidad_hospitalaria):
-        self.poblacion_total = poblacion_total
-        self.cant_contagio_inicial = cant_contagio_inicial
-        self.area_km2 = area_km2
-        self.capacidad_hospitalaria = capacidad_hospitalaria
-        self.id_simulacion = None
-
